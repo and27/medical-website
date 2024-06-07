59a02d023762becbd9ce4ad2d77795c13367886e
Andrés Banda <andrezh_gln7@live.com>
2024-06-07 17:49:40 -0500

    feat: add suport for passing icons as props in threeColummnItems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,45 +3,55 @@ import Hero from "@/components/Hero";
 import Nav from "@/components/Nav";
 import SCurve from "@/components/SCurve";
 import ThreeColumnItems from "@/components/ThreeColumnItems";
+import { FaUserMd, FaYinYang } from "react-icons/fa";
+import { FaSpa } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Hero />
-      <section className="text-center my-[8rem]">
+      <section
+        className="text-center my-[8rem] max-w-[1280px] mx-auto"
+        id="benefits"
+      >
         <h2 className="text-4xl text-slate-700 my-[5rem] font-semibold w-96 mx-auto">
           ¿Porqué deberías confiar en nosotros?
         </h2>
         <ThreeColumnItems
           items={[
             {
-              title: "Item 1",
-              description: "Description 1",
-              href: "https://example.com",
+              title: "Enfoque Holístico",
+              description:
+                "Nuestro enfoque holístico asegura que consideramos todos los aspectos de tu salud, incluyendo el bienestar físico, emocional y mental.",
+              icon: FaUserMd,
             },
             {
-              title: "Item 2",
-              description: "Description 2",
-              href: "https://example.com",
+              title: "Ambiente Acogedor",
+              description:
+                "Nuestro consultorio está diseñado para proporcionar un ambiente tranquilo y acogedor donde puedas sentirte relajado y cómodo. ",
+              icon: FaSpa,
             },
             {
-              title: "Item 3",
-              description: "Description 3",
-              href: "https://example.com",
+              title: "Terapias Personalizadas",
+              description:
+                "Entendemos que cada persona es única, por eso adaptamos nuestros tratamientos a tus necesidades específicas.",
+              icon: FaYinYang,
             },
           ]}
         />
       </section>
 
-      <section className="text-center mt-[10rem]">
-        <h2 className="text-4xl text-slate-700 my-[5rem] font-semibold w-96 mx-auto">
-          ¿Cuáles son nuestros servicios?
-        </h2>
-        <SCurve direction="right" />
-        <SCurve direction="left" />
-        <SCurve direction="right" />
-      </section>
+      <div className="bg-white pt-[4rem]">
+        <section className="text-center  max-w-[1280px] mx-auto" id="services">
+          <h2 className="text-4xl text-slate-700 my-[5rem] font-semibold w-96 mx-auto">
+            ¿Cuáles son nuestros servicios?
+          </h2>
+          <SCurve direction="right" />
+          <SCurve direction="left" />
+          <SCurve direction="right" />
+        </section>
+      </div>
 
       <Footer />
     </>
--- src/components/ThreeColumnItems.tsx
+++ src/components/ThreeColumnItems.tsx
@@ -1,32 +1,33 @@
-import { IconBase } from "react-icons";
-import { FaBeer } from "react-icons/fa";
-import { FaBabyCarriage } from "react-icons/fa6";
+import { IconType } from "react-icons";
 
 interface ThreeColumnItemsProps {
   items: {
     title: string;
     description: string;
-    href: string;
+    icon: IconType;
   }[];
 }
 
 const ThreeColumnItems = ({ items }: ThreeColumnItemsProps) => {
   return (
     <div className="grid lg:mb-0 lg:max-w-5xl lg:grid-cols-3 mx-auto gap-5">
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center gap-3 shadow-lg p-5 bg-white"
-        >
-          <div className="flex items-center justify-center w-20 h-20 rounded-full bg-orange-500 p-5">
-            <FaBabyCarriage size={50} color="white" />
+      {items.map((item, index) => {
+        const Icon = item.icon;
+        return (
+          <div
+            key={index}
+            className="flex flex-col items-center gap-4 shadow hover:shadow-lg p-7 bg-white"
+          >
+            <div className="flex items-center justify-center w-20 h-20 rounded-full bg-teal-500 p-5">
+              <Icon size={50} color="white" />
+            </div>
+            <h3 className="text-2xl font-semibold text-slate-700">
+              {item.title}
+            </h3>
+            <p className="text-slate-500">{item.description}</p>
           </div>
-          <h3 className="text-2xl font-semibold text-slate-800">
-            {item.title}
-          </h3>
-          <p className="text-slate-600">{item.description}</p>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
