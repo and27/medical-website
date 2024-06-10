ed6b4dce7e30bd1eeaed82b9299eb95f58f65fd7
Andrés Banda <andrezh_gln7@live.com>
2024-06-10 14:16:59 -0500

    feat: update footer with new content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
     <footer className="bg-slate-900 text-white text-center py-12">
-      <p className="m-0">© 2021 - 2024. All rights reserved.</p>
+      <p className="mb-2">© Iván 2021 - 2024. Todos los derechos reservados.</p>
+      <p className="text-sm ">Creado con ❤️ por Andres B.</p>
     </footer>
   );
 };
